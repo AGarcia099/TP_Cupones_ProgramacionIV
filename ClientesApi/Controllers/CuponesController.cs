d7f4cd9385b909779cc7e893eb25fd2cb4c9c64e
fabristein <fabrizio.steinbach@gmail.com>
2024-11-15 20:15:14 -0300

    articulocategoriascontroller

--- ClientesApi/Controllers/CuponesController.cs
+++ ClientesApi/Controllers/CuponesController.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
+using ClientesApi.Interfaces;
+using ClientesApi.Models;
+using ClientesApi.Services;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -10,23 +13,41 @@ public class CuponesController : ControllerBase
 {
     private readonly HttpClient _httpClient;
 
-    public CuponesController(HttpClient httpClient)
+    private readonly IClienteService _clienteService;
+
+    public CuponesController(HttpClient httpClient, IClienteService clienteService)
     {
         _httpClient = httpClient;
+        _clienteService = clienteService;
     }
 
+    
     [HttpPost("ReclamarCupon")]
-    public async Task<IActionResult> ReclamarCupon([FromBody] ClienteDto clienteDto)
+    public async Task<IActionResult> EnviarSolicitudACupones([FromBody] ClienteDto clienteDto)
     {
-        var response = await _httpClient.PostAsJsonAsync("https://localhost:7024/api/SolicitudCupones/SolicitarCupon", clienteDto);
-
-        if (response.IsSuccessStatusCode)
+        try
         {
-            return Ok(await response.Content.ReadFromJsonAsync<object>());
+            var respuesta = await _clienteService.SolicitarCupon(clienteDto);
+            return Ok(respuesta);
         }
-
-        return BadRequest($"Error: {response.ReasonPhrase}");
+        catch (Exception ex)
+        {
+            return BadRequest($"Error: {ex.Message}");
+        }
     }
+
+    //[HttpPost("ReclamarCupon")]
+    //public async Task<IActionResult> ReclamarCupon([FromBody] ClienteDto clienteDto)
+    //{
+    //    var response = await _httpClient.PostAsJsonAsync("https://localhost:7024/api/SolicitudCupones/SolicitarCupon", clienteDto);
+
+    //    if (response.IsSuccessStatusCode)
+    //    {
+    //        return Ok(await response.Content.ReadFromJsonAsync<object>());
+    //    }
+
+    //    return BadRequest($"Error: {response.ReasonPhrase}");
+    //}
 
     [HttpPost("UsarCupon")]
     public async Task<IActionResult> UsarCupon([FromBody] string nroCupon)
---
+++ CuponesApiTp/Controllers/ArticuloCategoriaController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CuponesApiTp.Data;
+using CuponesApiTp.Models;
+
+namespace CuponesApiTp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticuloCategoriaController : Controller
+    {
+
+        private readonly DataBaseContext _context;
+
+        public ArticuloCategoriaController(DataBaseContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddArticuloToCategoria(int idArticulo, int idCategoria)
+        {
+            try
+            {
+                // Buscar el artículo por su Id
+                var articulo = await _context.Articulos
+                    .FirstOrDefaultAsync(a => a.Id_Articulo == idArticulo);
+
+                if (articulo == null)
+                {
+                    return NotFound("El artículo no existe o no está activo.");
+                }
+
+                // Validar si la categoría existe
+                var categoria = await _context.Categorias
+                    .FirstOrDefaultAsync(c => c.Id_Categoria == idCategoria);
+
+                if (categoria == null)
+                {
+                    return NotFound("La categoría no existe.");
+                }
+
+                // Crear la relación entre artículo y categoría
+                var cuponCategoria = new Cupon_CategoriaModel
+                {
+                    Id_Cupon = articulo.Id_Articulo,
+                    Id_Categoria = idCategoria,
+                };
+
+                _context.Cupones_Categorias.Add(cuponCategoria);
+                await _context.SaveChangesAsync();
+
+                return Created("AddArticuloToCategoria", new
+                {
+                    message = $"El artículo '{articulo.Nombre_Articulo}' fue agregado exitosamente a la categoría '{categoria.Nombre}'."
+                });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Ocurrió un error al asignar el artículo a la categoría. Detalles: {ex.Message}");
+            }
+        }
+
+
+
+    }
+}
